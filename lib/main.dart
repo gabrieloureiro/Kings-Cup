24b293d8daa1430db201fce7269ba5ed88ac6fb2
Gabriel Loureiro <gabrieloureirof@gmail.com>
2019-10-25 22:49:28 -0300

    Ready for release

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:kingsCup/cards.dart';
+import 'package:material_design_icons_flutter/material_design_icons_flutter.dart';
 import 'package:url_launcher/url_launcher.dart';
 
 void main() => runApp(Home());
@@ -31,79 +32,86 @@ _launchURL(String url) async {
 }
 
 class _HomeState extends State<HomeScreen> {
-@override
+  @override
   Widget build(BuildContext context) {
     return Stack(
-      
       children: <Widget>[
-        Image.network(
-          "https://media.giphy.com/media/3ov9jUCYetT3GVwcy4/giphy.gif",
+        Image.asset(
+          "images/icons/hsc.gif",
           fit: BoxFit.cover,
           height: double.maxFinite,
-          width: double.maxFinite, 
+          width: double.maxFinite,
           alignment: Alignment.center,
         ),
         Scaffold(
           backgroundColor: Colors.transparent,
           appBar: AppBar(
             backgroundColor: Colors.transparent,
-            title: Text(
-              "KING'S CUP",
-              style: TextStyle(
-                fontSize: 20,
-                fontWeight: FontWeight.bold,
-                color : Colors.white,
-              ),
+            title: Image.asset(
+              "images/icons/homebar.png",
+              width: 108,
             ),
             centerTitle: true,
           ),
           body: Container(
+              child: Row(
+            children: <Widget>[],
+          )),
+          //BARRA DE NAVEGAÇÃO INFERIOR
+          // bottomNavigationBar: BottomNavigationBar(
+          //   color: Colors.white,
+          //   onPressed: (){
+          // })
+          floatingActionButtonLocation: FloatingActionButtonLocation.centerDocked,
+          floatingActionButton: FloatingActionButton(
+            backgroundColor: Colors.red,
+            child:  IconButton(
+                  icon: Icon(Icons.play_arrow),
+                  
+                  //color: Colors.red,
+                  //iconSize: 100,
+                  tooltip: "JOGAR",
+                  onPressed: () {
+                    Navigator.push(
+                      context,
+                      MaterialPageRoute(builder: (context) => Cards()),
+                    );
+                  },
+                ),
+            onPressed: () {},
+          ),
+          bottomNavigationBar: BottomAppBar(
+            shape: CircularNotchedRectangle(),
+            notchMargin: 4.0,
             child: Row(
+              mainAxisSize: MainAxisSize.max,
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
               children: <Widget>[
                 IconButton(
-                  icon: Icon(Icons.developer_mode),
+                  icon: Icon(MdiIcons.githubCircle),
                   color: Colors.red,
-                  iconSize: 100,
+                  //iconSize: 100,
                   tooltip: "Conheça os desenvolvedores",
                   alignment: Alignment.centerRight,
                   onPressed: () {
                     _launchURL("http://github.com/gabrieloureiro");
                   },
                 ),
+                
+               
                 IconButton(
                   icon: Icon(Icons.share),
                   color: Colors.red,
-                  iconSize: 100,
+                  //iconSize: 100,
                   tooltip: "Compartilhe o aplicativo",
                   alignment: Alignment.centerRight,
-                  onPressed: () {
-                    
-                  },
+                  onPressed: () {},
                 ),
-                IconButton(
-                  icon: Icon(Icons.play_arrow),
-                  color: Colors.red,
-                  iconSize: 100,
-                  tooltip: "JOGAR",
-                  alignment: Alignment.centerRight,
-                  onPressed: () {
-                    Navigator.push(context,
-                      MaterialPageRoute(
-                      builder: (context) => Cards()
-                      ),
-                    );
-                  },
-                )
               ],
-            ) 
+            ),
           ),
-          //BARRA DE NAVEGAÇÃO INFERIOR
-          // bottomNavigationBar: BottomNavigationBar(
-          //   color: Colors.white,
-          //   onPressed: (){
-          // })       
-
-      )
-    ],);
+        )
+      ],
+    );
   }
 }
